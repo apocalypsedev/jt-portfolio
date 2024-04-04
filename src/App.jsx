482bfb7a566685b9f401f6d3a218fdfbd232cc78
Husanbek <buxorojahon@gmail.com>
2024-04-04 10:41:05 +0300

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <About/>
+      <Skills/>
+      <Footer/>
     </>
   )
 }
